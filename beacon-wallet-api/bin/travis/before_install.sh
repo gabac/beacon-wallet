c7b5f13d0f6f760f6df555c6d5483167ecdcb4ae
Fabian Vogler <fabian@equivalence.ch>
2014-05-12 20:30:08 +0200

    Install Composer for Travis

--- beacon-wallet-api/bin/travis/before_install.sh
+++ beacon-wallet-api/bin/travis/before_install.sh
@@ -6,9 +6,10 @@ set -e -x
 # change to working dir
 cd `dirname $0`/../..
 
-brew update
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap homebrew/homebrew-php
-brew install php55
+#brew update
+#brew tap homebrew/dupes
+#brew tap homebrew/versions
+#brew tap homebrew/homebrew-php
+#brew install php55
+curl -sS https://getcomposer.org/installer | php
 composer self-update
