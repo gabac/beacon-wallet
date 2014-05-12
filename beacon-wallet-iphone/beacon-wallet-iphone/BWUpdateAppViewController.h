dfb2afbc2701aeb080264bd354785554518b34b2
gabac <gabathuler@gmail.com>
2014-05-12 18:57:06 +0200

    update view

---
+++ beacon-wallet-iphone/beacon-wallet-iphone/BWUpdateAppViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWUpdateAppViewController.h
+//  beacon-wallet-iphone
+//
+//  Created by Cyril Gabathuler on 12.05.14.
+//  Copyright (c) 2014 Bahnhofstrasse24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWUpdateAppViewController : UIViewController
+
+@end
---
+++ beacon-wallet-iphone/beacon-wallet-iphone/BWUpdateAppViewController.m
@@ -0,0 +1,42 @@
+//
+//  BWUpdateAppViewController.m
+//  beacon-wallet-iphone
+//
+//  Created by Cyril Gabathuler on 12.05.14.
+//  Copyright (c) 2014 Bahnhofstrasse24. All rights reserved.
+//
+
+#import "BWUpdateAppViewController.h"
+
+@interface BWUpdateAppViewController ()
+
+@end
+
+@implementation BWUpdateAppViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+- (IBAction)donePressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
