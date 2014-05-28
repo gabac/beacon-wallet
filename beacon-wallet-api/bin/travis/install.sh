f641c93457c469a0d73c97c17d00dc26fae2195d
Fabian Vogler <fabian@equivalence.ch>
2014-05-28 22:00:38 +0200

    Use root MySQL user for Travis

--- beacon-wallet-api/bin/travis/install.sh
+++ beacon-wallet-api/bin/travis/install.sh
@@ -7,7 +7,7 @@ set -e -x
 cd `dirname $0`/../..
 
 php composer.phar install --prefer-source
-cp config.php.travis config.php
+cp config.php.dist config.php
 mysql -e 'create database beacon_wallet;'
 mysql --database=beacon_wallet < sql/install.sql
 mysql --database=beacon_wallet < sql/test.sql
