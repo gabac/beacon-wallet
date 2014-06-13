67a5544ea13cd0c55e15fe7e1d60d1cf03c758c0
Fabian Vogler <fabian@equivalence.ch>
2014-06-13 08:44:48 +0200

    Set date.timezone for Travis

--- beacon-wallet-api/bin/travis/before_install.sh
+++ beacon-wallet-api/bin/travis/before_install.sh
@@ -9,3 +9,5 @@ cd `dirname $0`/../..
 brew update
 brew install mysql
 mysql.server start
+
+echo 'date.timezone = "Europe/Zurich"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
