9510e960a7cb17354bde3151f3052a7f1ecbfe17
Fabian Vogler <fabian@equivalence.ch>
2014-06-17 21:39:31 +0200

    Added view for iPad

--- beacon-wallet-ipad/beacon-wallet-ipad/BWAppDelegate.m
+++ beacon-wallet-ipad/beacon-wallet-ipad/BWAppDelegate.m
@@ -12,7 +12,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
     NSUUID *cashierUUID = [[NSUUID alloc] initWithUUIDString:@"11111111-1111-1111-1111-111111111111"];
@@ -25,9 +24,6 @@
     self.peripheralManager.delegate = self;
     [self.peripheralManager startAdvertising:peripheralDictionary];
     
-    
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
---
+++ beacon-wallet-ipad/beacon-wallet-ipad/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Fabian on 17.06.14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
