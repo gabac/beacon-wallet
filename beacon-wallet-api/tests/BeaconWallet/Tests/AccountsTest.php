5dd2ddca93e581edb3e4385723281f24934566a1
Fabian Vogler <fabian@equivalence.ch>
2014-06-17 17:49:16 +0200

    Test getAccount without and with wrong authentication

--- beacon-wallet-api/tests/BeaconWallet/Tests/AccountsTest.php
+++ beacon-wallet-api/tests/BeaconWallet/Tests/AccountsTest.php
@@ -15,4 +15,25 @@ class AccountsTest extends ApiTest
         $this->assertTrue($client->getResponse()->isOk());
         $this->assertJsonStringEqualsJsonFile(__DIR__ . '/fixtures/getAccount.json', $client->getResponse()->getContent());
     }
+
+    /**
+     * @expectedException Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+     */
+    public function testGetAccountUnauthenticated()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/accounts/2501032235098');
+    }
+
+    /**
+     * @expectedException Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+     */
+    public function testGetAccountInvalidAuthentication()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/accounts/2501032235098', array(), array(), array(
+            'PHP_AUTH_USER' => '2501032235098',
+            'PHP_AUTH_PW'   => '9999',
+        ));
+    }
 }
