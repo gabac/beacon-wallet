3bf89577ffa7ac1f1bd25c6f0c1b30755dd9c610
Fabian Vogler <fabian@equivalence.ch>
2014-06-18 20:05:10 +0200

    Refactored AccountsController

--- beacon-wallet-api/src/BeaconWallet/Controller/TransactionsController.php
+++ beacon-wallet-api/src/BeaconWallet/Controller/TransactionsController.php
@@ -54,28 +54,55 @@ class TransactionsController
 
         $data = json_decode($decrypted);
 
+        // read parameters from decoded request
         $card = isset($data->card) ? $data->card : null;
         $products = isset($data->products) && is_array($data->products) ? $data->products : array();
 
         // create transaction
         $transactionId = $this->transactions->createTransaction($card, $products);
 
-        // prepare transaction response
-        $json = $this->getTransactionJson($transactionId);
-
-        // sign response
-        $signature = $this->crypt->sign($json);
-
-        return new JsonResponse(array(
-            'transaction' => $json,
-            'signature' => $signature,
-        ));
+        // return transaction response
+        return $this->getTransactionResponse($transactionId);
     }
 
-    protected function getTransactionJson($transactionId)
+    /**
+     * Complete transaction with payment.
+     */
+    public function payTransaction(Request $request)
     {
+        $encrypted = $request->get('payment');
+
+        // decrypt payment
+        $decrypted = $this->crypt->decrypt($encrypted);
+
+        $data = json_decode($decrypted);
+
+        // read parameters from decoded request
+        $id = isset($data->id) ? $data->id : null;
+        $card = isset($data->card) ? $data->card : null;
+        $pin = isset($data->pin) ? $data->pin : null;
+
+        // security check
+        if (!$this->accounts->verifyPin($card, $pin)) {
+            throw new AccessDeniedHttpException('Invalid card and pin');
+        }
+
+        // complete transaction
+        $this->transactions->payTransaction($id, $card);
+
+        // return transaction response
+        return $this->getTransactionResponse($id);
+    }
+
+    /**
+     * Internal method to prepare transaction JSON response with a signature.
+     */
+    protected function getTransactionResponse($transactionId)
+    {
+        // fetch transaction from storage
         $transaction = $this->transactions->getTransaction($transactionId);
 
+        // prepare transaction response
         $data = array(
             'id' => (int) $transaction['id'],
             'status' => $transaction['status'],
@@ -84,45 +111,25 @@ class TransactionsController
             'products' => array(),
         );
 
+        // loop products
         foreach ($transaction['products'] as $product) {
 
+            // add product to transaction response
             $data['products'][] = array(
                 'id' => (int) $product['product'],
                 'quantity' => (int) $product['quantity'],
                 'amount' => (float) $product['amount'],
             );
 
+            // add product amount to total
             $data['amount'] += (float) $product['amount'];
         }
 
-        return json_encode($data);
-    }
-
-    public function payTransaction(Request $request)
-    {
-        // encrypt payment
-        $encrypted = $request->get('payment');
-
-        $decrypted = $this->crypt->decrypt($encrypted);
-
-        $data = json_decode($decrypted);
-
-        $id = isset($data->id) ? $data->id : null;
-        $card = isset($data->card) ? $data->card : null;
-        $pin = isset($data->pin) ? $data->pin : null;
-
-        if (!$this->accounts->verifyPin($card, $pin)) {
-            throw new AccessDeniedHttpException('Invalid card and pin');
-        }
-
-        $this->transactions->payTransaction($id, $card);
-
-        // prepare transaction response
-        $json = $this->getTransactionJson($id);
+        $json = json_encode($data);
 
         // sign response
         $signature = $this->crypt->sign($json);
-        
+
         return new JsonResponse(array(
             'transaction' => $json,
             'signature' => $signature,
